245b9976257154d7b5083513ae85548428de87ab
Pham Thai Binh <thaibinh100402@gmail.com>
2024-02-29 14:48:59 +0700

    NodeJs

--- NodeJs/src/controller/homeController.js
+++ NodeJs/src/controller/homeController.js
@@ -1,6 +1,14 @@
+import db from '../models/index'
+let getHomePage = async (req, res) => {
+    try {
+        let data = await db.User.findAll({ raw: true });
 
-let getHomePage = (req, res) => {
-    return res.send('Phạm Thái Bình')
+        return res.render('homepage.ejs', {
+            data: JSON.stringify(data)        // truyền data ra views //chuyển data thành kiểu string
+        })
+    } catch (e) {
+        console.log(e);
+    }
 }
 
 module.exports = {
